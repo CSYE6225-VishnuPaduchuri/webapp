812dc43dd63e197b8307c3c03ec7003586fcf3fe
Vishnu Paduchuri <paduchuri.v@northeastern.edu>
2024-02-14 23:04:52 -0500

    Added 2 test cases to verify POST and PUT calls for the user API

---
+++ tests/user.test.js
@@ -0,0 +1,88 @@
+import assert from "assert";
+import request from "supertest";
+import app from "../server.js";
+import { EncodeUserCredentials } from "../utils/common/index.js";
+
+let localServer;
+
+const userPayload = {
+  first_name: "Vishnu",
+  last_name: "Kohli",
+  username: "virat7@gmail.com",
+  password: "Vk@123",
+};
+
+const updateUserPayload = {
+  first_name: "Virat",
+};
+
+// this is a hook that runs before running any testcases
+before((done) => {
+  localServer = app.listen(() => {
+    done();
+  });
+});
+
+// this is a hook that runs after running all the testcases
+after((done) => {
+  localServer.close(() => {
+    console.log("Server stopped aftet all tests run");
+    done();
+  });
+});
+
+// `describe` function is used to explain why we are running the test case
+describe("Create a new user and call get to verify if user has been created", () => {
+  it("a user has to be created and their record has to be present in the Database", async () => {
+    const createdUserResponse = await request(localServer)
+      .post("/v1/user")
+      .send(userPayload);
+
+    // we are checking if the request was successful by checking if the status code is 201
+    assert.strictEqual(createdUserResponse.status, 201);
+
+    const getUserDetailsRes = await request(localServer)
+      .get("/v1/user/self")
+      .set(
+        "Authorization",
+        EncodeUserCredentials(userPayload.username, userPayload.password)
+      );
+
+    // we are checking if the request was successful by checking if the status code is 200
+    assert.strictEqual(getUserDetailsRes.status, 200);
+    assert.strictEqual(getUserDetailsRes.body.username, userPayload.username);
+  });
+});
+
+// `describe` function is used to explain why we are running the below test case
+describe("Create a new user and call get to verify if user has been created", () => {
+  it("a user has to be created and their record has to be present in the Database", async () => {
+    const updatedUserResponse = await request(localServer)
+      .put("/v1/user/self")
+      .set(
+        "Authorization",
+        EncodeUserCredentials(userPayload.username, userPayload.password)
+      )
+      .send(updateUserPayload);
+
+    // since we are sending 204 from our server, we will check for status code 204
+    assert.strictEqual(updatedUserResponse.status, 204);
+
+    const getUserDetailsRes = await request(localServer)
+      .get("/v1/user/self")
+      .set(
+        "Authorization",
+        EncodeUserCredentials(userPayload.username, userPayload.password)
+      );
+
+    // we are checking if the request was successful by checking if the status code is 200
+    assert.strictEqual(getUserDetailsRes.status, 200);
+
+    // here we are checking if the first_name field has been updated
+    // we are checking between the payload sent to PUT call and the response from GET call
+    assert.strictEqual(
+      getUserDetailsRes.body.first_name,
+      updateUserPayload.first_name
+    );
+  });
+});
