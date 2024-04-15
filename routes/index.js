e124f9fbea16dbed173ab9e840435a13c59c050b
Vishnu Paduchuri <paduchuri.v@northeastern.edu>
2024-04-15 19:54:56 -0400

    Change version number

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ export default (app) => {
   app.use("/healthz", databaseRouter);
 
   // For all API calls to /v1/user, userRouter will be handling the request
-  app.use("/v1/user", userRouter);
+  app.use("/v2/user", userRouter);
 
   // If someone hits a path that doesn't exist then we are sending 404 status
   app.use((req, res) => {
